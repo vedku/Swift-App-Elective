ff1bf270c734a00f168ba5f2937d01e6c01a97ea
vedku <57610056+vedku@users.noreply.github.com>
2022-09-07 18:51:22 +0800

    Update v1.swift

--- Flaschard App/v1.swift
+++ Flaschard App/v1.swift
@@ -1,19 +1,31 @@
-//doesn't work yet lmao
+//ideally would allow the user to input stuff in the UI, lock answer buttons. all coming in v2
 import SwiftUI
 let prompts = ["Formula for Force?", "Avogadro's Constant?", "Kinetic Energy Formula?", "What is the formula for Weight?", "How does Weight differ from Mass?", "Units for Mass?", "Is momentum a vector?"]
+let answers = ["F=Ma", "6.023x10^23", "1/2Mv^2", "W=Mg", "Weight is measured in Newtons and is a Vector, whereas Mass is a scalar and measured in Kilograms", "Kilograms", "Yes"]
 struct ContentView: View {
     @State var prompt_num = 0 
+    @State var answer_num = 0
     var body: some View {
         VStack {
-            Text(prompts[0])
+            Text(prompts[prompt_num])
                 .font(.title)
                 .padding()
             Button("New Prompt"){
-                prompt_num = prompt_num += 1
+                prompt_num = prompt_num + 1
             }
             .buttonStyle(.bordered)
             .padding()
-            
+
+        }
+        VStack {
+            Text(answers[answer_num])
+                .font(.title)
+                .padding()
+            Button("Show Answer"){
+                answer_num = answer_num + 1
+            }
+            .buttonStyle(.bordered)
+            .padding()
         }
     }
 }
