fdb2536a05b253f191c6dc0f543510770d025eba
vedku <57610056+vedku@users.noreply.github.com>
2022-09-13 17:06:52 -0700

    Update v1.swift

--- Converter/v1.swift
+++ Converter/v1.swift
@@ -1,1 +1,11 @@
-//.
+import SwiftUI
+struct ContentView: View {
+    @State var km = 0.00
+    @State var mile = 0.00
+    var body: some View {
+        HStack {
+            TextField("Quantity 1", value: $km, format: .number)
+            Text(String(km*0.62137))
+        }
+    }
+}
