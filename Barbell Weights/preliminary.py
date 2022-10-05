a4458b12e00ec6f2b87c18f2f17bd3a9dca2e345
vedku <57610056+vedku@users.noreply.github.com>
2022-10-04 17:43:48 -0700

    Update preliminary.py

--- Barbell Weights/preliminary.py
+++ Barbell Weights/preliminary.py
@@ -7,7 +7,7 @@ while True:
     tenlbsplate = 10
     fivelbsplate = 5
     twopointfivelbsplate = 2.5
-    W2A = 0 #W2A stands for weight to add
+    W2A = 0
 
     #the number of times a plate is used
     n45lbs = 0
@@ -17,28 +17,20 @@ while True:
     n5lbs = 0
     n2pt5lbs = 0
 
-    weight = int(input("How much weight do you want to lift?:"))
+    weight = float(input("How much weight do you want to lift?:"))
     weight = weight-barbell #we have now accommodated for the 45lb barbell
 
-    W2A = weight/fortyfivelbsplate
-    if W2A < 2:
-        W2A = weight/thirtyfivelbsplate
-    else:
-        n45lbs  = n45lbs + 2
-        if W2A < 2:
-            W2A = weight/twentyfivelbsplate
-        else:
-            n35lbs = n35lbs + 2
-            if W2A <2:
-                W2A = weight/tenlbsplate
-            else:
-                n25lbs = n25lbs + 2
-                if W2A < 2:
-                    W2A = weight/fivelbsplate
-                else:
-                    n10lbs = n10lbs = n10lbs + 2
-                    if W2A < 2:
-                        W2A = weight/twopointfivelbsplate
-
-    #print("You need to add", n45lbs, "45lb plates,", n35lbs, "35lb plates,", n25lbs,  "25lb plates,", n10lbs, "10lb plates,", n5lbs, "5lb plates, and", n2pt5lbs, "2.5lb plates.")
-    print(W2A)
+    n45lbs = int(str(weight/90)[0])
+    weight -= n45lbs*90
+    n35lbs = int(str(weight/70)[0])
+    weight -= n35lbs*70
+    n25lbs = int(str(weight/50)[0])
+    weight -= n25lbs*50
+    n10lbs = int(str(weight/20)[0])
+    weight -= n10lbs*20
+    n5lbs = int(str(weight/10)[0])
+    weight -= n5lbs*10
+    n2pt5lbs = int(str(weight/5)[0])
+    weight -= n2pt5lbs*5
+    
+    print("You need to add", n45lbs, "45lb plates on either side,", n35lbs, "35lb plates on either side,", n25lbs,  "25lb plates on either side,", n10lbs, "10lb plates on either side,", n5lbs, "5lb plates on either side, and", n2pt5lbs, "2.5lb plates on either side.")
