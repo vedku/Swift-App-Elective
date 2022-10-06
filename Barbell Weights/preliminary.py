2d6f38841e9243c408632ba5fbaa3a6d0f21538d
vedku <57610056+vedku@users.noreply.github.com>
2022-10-06 08:07:59 +0800

    Update preliminary.py

--- Barbell Weights/preliminary.py
+++ Barbell Weights/preliminary.py
@@ -1,7 +1,5 @@
 while True:
 
-    barbell = 45
-
     #the number of times a plate is used
     n45lbs = 0
     n35lbs = 0
@@ -11,7 +9,7 @@ while True:
     n2pt5lbs = 0
 
     weight = float(input("How much weight do you want to lift?:"))
-    weight = weight-barbell #we have now accommodated for the 45lb barbell
+    weight = weight-45 #we have now accommodated for the 45lb barbell
 
     n45lbs = int(str(weight/90)[0])
     weight -= n45lbs*90
@@ -26,9 +24,9 @@ while True:
     n2pt5lbs = int(str(weight/5)[0])
     weight -= n2pt5lbs*5
 
-    print("You need to add", n45lbs, "45lb plates on either side")
-    print("You need to add", n35lbs, "35lb plates on either side")
-    print("You need to add", n25lbs, "25lb plates on either side")
-    print("You need to add", n10lbs, "10lb plates on either side")
-    print("You need to add", n5lbs, "5lb plates on either side")
-    print("You need to add", n2pt5lbs, "2.5lb plates on either side")
+    print("You need to add", n45lbs, "45lb plate(s) on either side")
+    print("You need to add", n35lbs, "35lb plate(s) on either side")
+    print("You need to add", n25lbs, "25lb plate(s) on either side")
+    print("You need to add", n10lbs, "10lb plate(s) on either side")
+    print("You need to add", n5lbs, "5lb plate(s) on either side")
+    print("You need to add", n2pt5lbs, "2.5lb plate(s) on either side")
