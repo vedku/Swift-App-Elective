66ca95fb942c0d18f9aef4c71d0fdd0b089c45f0
vedku <57610056+vedku@users.noreply.github.com>
2022-10-25 17:19:31 -0700

    Update barbellv1.swift

--- Barbell Weights/Swift/barbellv1.swift
+++ Barbell Weights/Swift/barbellv1.swift
@@ -1,1 +1,22 @@
-//
+//incomplete
+import SwiftUI
+struct ContentView: View {
+    @State var n45lbs = 0
+    @State var n35lbs = 0
+    @State var n25lbs = 0
+    @State var n10lbs = 0
+    @State var n5lbs = 0
+    @State var n2pt5lbs = 0
+    @State var weight = []
+    
+    var body: some View{
+        VStack{
+            Text("How much weight do you want to lift?")
+                .font(.title    )
+        }
+        HStack{
+            TextField("Input here", value: $n45lbs, format: .number)
+            Text(String())
+        }
+    }
+}
