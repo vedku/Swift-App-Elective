a53db7086bb82b605dfc74f64c277d4a95aeb99d
vedku <57610056+vedku@users.noreply.github.com>
2022-11-01 17:56:08 -0700

    Update barbellv1.swift

--- Barbell Weights/Swift/barbellv1.swift
+++ Barbell Weights/Swift/barbellv1.swift
@@ -1,13 +1,13 @@
 import SwiftUI
 struct ContentView: View {
-    @State var n45lbs = 0.00
-    @State var n35lbs = 0.00
-    @State var n25lbs = 0.00
-    @State var n10lbs = 0.00
-    @State var n5lbs = 0.00
-    @State var n2pt5lbs = 0.00
-    @State var weight = 0.00
-    @State var weightCalc = 0.00
+    @State var n45lbs = 0
+    @State var n35lbs = 0
+    @State var n25lbs = 0
+    @State var n10lbs = 0
+    @State var n5lbs = 0
+    @State var n2pt5lbs = 0
+    @State var weight = 0
+    @State var weightCalc = 0
     
     var body: some View {
         VStack {
@@ -27,25 +27,26 @@ struct ContentView: View {
                 weightCalc = weight
                 weightCalc = weightCalc - 45
                 
-                n45lbs = weightCalc/45
-                weightCalc = weightCalc - n45lbs*45
+                n45lbs = weightCalc/90
+                weightCalc = weightCalc - n45lbs*90
                 
-                n35lbs = weightCalc/35
-                weightCalc = weightCalc - n35lbs*35
+                n35lbs = weightCalc/70
+                weightCalc = weightCalc - n35lbs*70
                 
-                n25lbs = weightCalc/25
-                weightCalc = weightCalc - n25lbs*25
+                n25lbs = weightCalc/50
+                weightCalc = weightCalc - n25lbs*50
                 
-                n10lbs = weightCalc/10
+                n10lbs = weightCalc/20
                 weightCalc = weightCalc - n10lbs*10
                 
-                n5lbs = weightCalc/5
-                weightCalc = weightCalc - n5lbs*5
+                n5lbs = weightCalc/10
+                weightCalc = weightCalc - n5lbs*10
                 
-                n2pt5lbs = weightCalc/2.5
-                weightCalc = weightCalc - n2pt5lbs*2.5
+                n2pt5lbs = weightCalc/5
+                weightCalc = weightCalc - n2pt5lbs*5
             }
-            HStack {
+            VStack {
+                Text("You need to add")
                 Text(String(n45lbs))
                 Text(String(n35lbs))
                 Text(String(n25lbs))
