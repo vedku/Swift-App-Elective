3d71a2b63dc1d4a1079c4e0dc456131d3de6aaee
vedku <57610056+vedku@users.noreply.github.com>
2022-11-01 17:24:24 -0700

    Update barbellv1.swift

--- Barbell Weights/Swift/barbellv1.swift
+++ Barbell Weights/Swift/barbellv1.swift
@@ -7,43 +7,49 @@ struct ContentView: View {
     @State var n5lbs = 0.00
     @State var n2pt5lbs = 0.00
     @State var weight = 0.00
+    @State var weightCalc = 0.00
     
     var body: some View {
         VStack {
             Text("How much weight do you want to lift?")
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 .padding()
-        }
         
         HStack {
             TextField("Input here", value: $weight, format: .number)
-                .textFieldStyle(.roundedBorder)
-                .border(.green, width: 4)
-                .keyboardType(.decimalPad)
-            let n45lbs = weight/90
-            let weight = weight - n45lbs*90
+            .textFieldStyle(.roundedBorder)
+            .border(.green, width: 4)
+            .keyboardType(.decimalPad)
             
-            let n35lbs = weight/70
-            let weight = weight - n35lbs*70
             
-            let n25lbs = weight/50
-            let weight = weight - n25lbs*50
-            
-            let n10lbs = weight/20
-            let weight = weight - n10lbs*20
-            
-            let n5lbs = weight/10
-            let weight = weight - n5lbs*10
-            
-            let n2pt5lbs = weight/5
-            let weight = weight - n2pt5lbs*5
-            
-            Text(String(n45lbs))
-            Text(String(n35lbs))
-            Text(String(n25lbs))
-            Text(String(n10lbs))
-            Text(String(n5lbs))
-            Text(String(n2pt5lbs))
         }
+            Button("Calculate!"){
+                let n45lbs = weight/90
+                weight = weight - n45lbs*90
+                
+                let n35lbs = weight/70
+                weight = weight - n35lbs*70
+                
+                let n25lbs = weight/50
+                weight = weight - n25lbs*50
+                
+                let n10lbs = weight/20
+                weight = weight - n10lbs*20
+                
+                let n5lbs = weight/10
+                weight = weight - n5lbs*10
+                
+                let n2pt5lbs = weight/5
+                weight = weight - n2pt5lbs*5
+            }
+            HStack {
+                Text(String(n45lbs))
+                Text(String(n35lbs))
+                Text(String(n25lbs))
+                Text(String(n10lbs))
+                Text(String(n5lbs))
+                Text(String(n2pt5lbs))
+            }
+    }
     }
 }
